3c9a49dcff5eab8306a23ee7bc6b02f35cd5de97
kwh0163 <kwh0163@gmail.com>
2022-08-09 21:13:53 +0900

    초기 커밋.

---
+++ 소스.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main()
+{
+	// 자료형
+
+	/*
+	char Char = 'c'; //문자형
+	int Int = 100; //정수형
+	float Float = 1.3f; //실수형
+	*/
+
+	//대입 연산자 "="
+	
+	/*
+	//오른쪽 값을 왼쪽의 피연산자에 저장함
+	int box = 100;
+	printf("%d\n", box);
+	printf("Hello");
+	*/
+
+
+
+
+
+
+}
