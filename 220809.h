36576fcbb8cd41691d84952ceb2860635c60b51f
kwh0163 <kwh0163@gmail.com>
2022-08-10 21:43:12 +0900

    20220810

--- 220809.h
+++ 220809.h
@@ -1,14 +1,15 @@
 #pragma once
 #include<stdio.h>
 
+
 void main()
 {
 	// 자료형
 
 	/*
-	char Char = 'c'; //문자형
-	int Int = 100; //정수형
-	float Float = 1.3f; //실수형
+	char Char = 'c'; //문자형 1byte
+	int Int = 100; //정수형 4byte
+	float Float = 1.3f; //실수형 4byte
 	*/
 
 	//대입 연산자 "="
