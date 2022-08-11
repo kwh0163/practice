57fdbfe9918cd51f83b168cab636a0dba9f28f82
kwh0163 <kwh0163@gmail.com>
2022-08-11 21:44:51 +0900

    20220811

--- 220809.h
+++ 220809.h
@@ -1,23 +1,24 @@
 #pragma once
 #include<stdio.h>
 
-
+/*
 void main()
 {
 	// 자료형
 
-	/*
+	
 	char Char = 'c'; //문자형 1byte
 	int Int = 100; //정수형 4byte
 	float Float = 1.3f; //실수형 4byte
-	*/
+	
 
 	//대입 연산자 "="
 
-	/*
+	
 	//오른쪽 값을 왼쪽의 피연산자에 저장함
 	int box = 100;
 	printf("%d\n", box);
 	printf("Hello");
-	*/
+	
 }
+*/
