b16bab1c5b9db62d201bd6f5b6823f3e1ee487cd
kwh0163 <105360209+hotteok13@users.noreply.github.com>
2022-08-12 21:46:16 +0900

    20220812

---
+++ RSP(20220812).h
@@ -0,0 +1,60 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void RSP()
+{
+
+	int IsGameOver = 0;
+	srand((unsigned int)time(NULL));
+	int p;
+	int e;
+	while (!IsGameOver)
+	{
+		printf("1 - 가위\n2 - 바위\n3 - 보\n");
+		scanf_s("%d", &p);
+		e = rand();
+		e = (int)e % 3;
+		switch (e)
+		{
+		case 1:
+			printf("적 : 가위\n");
+			break;
+		case 2:
+			printf("적 : 바위\n");
+			break;
+		case 3:
+			printf("적 : 보\n");
+			break;
+		}
+		if (p == e)
+		{
+			printf("draw\n");
+		}
+		else if (p == 1 && e == 3)
+		{
+			printf("player win\n");
+		}
+		else if (p == 3 && e == 1)
+		{
+			printf("player lose\n");
+		}
+		else if (p < e)
+		{
+			printf("player lose\n");
+		}
+		else
+		{
+			printf("player win\n");
+		}
+		printf("again?\n1 - Yes\n2 = No\n");
+		scanf_s("%d", &p);
+
+		if (p == 2)
+		{
+			IsGameOver = 1;
+		}
+	}
+
+}
