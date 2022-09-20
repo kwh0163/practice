10fe1d3d44c50174ebdc61a1f9b8f11e4a8f352b
kwh0163 <kwh0163@gmail.com>
2022-09-20 21:45:28 +0900

    20220920

---
+++ 220919.h
@@ -0,0 +1,104 @@
+#pragma once
+#include<stdio.h>
+
+struct Object
+{
+	short size; //2byte
+	int height; //4byte
+	double position; //8byte
+
+};
+
+struct Monster
+{
+	int healte; //4byte
+	double attack; //8byte
+	short defense; //2byte
+};
+
+struct Data
+{
+	int x; //4byte
+	int y; //4byte
+};
+
+struct Shape
+{
+	char name[10]; //10byte
+	double size; //8byte
+};
+
+struct Animal
+{
+	int leg;
+	double size;
+};
+
+int main()
+{
+
+	//바이트 패딩
+	/*
+	//구조체의 경우 정의해주어야 멤버 변수의 메모리가 생기므로
+	//멤버 변수의 데이터를 초기화할 수 있습니다.
+	struct Object character = { 10, 100, 3.5f };
+	struct Data data = { 10, 10 };
+
+	//구조체 크기의 경우 멤버 변수의 순서에 따라 메모리의 크기가
+	//다르게 설정될 수 있으며, 구조체 크기를 결정하는 형태는
+	//기본 자료형으로만 구성됩니다.
+	struct Monster dragon = { 100,30.5f,10 };
+	struct Shape circle;
+
+	printf("Object 구조체의 크기 : %d\n", sizeof(character));
+	printf("Data 구조체의 크기 : %d\n", sizeof(data));
+	printf("Monster 구조체의 크기 : %d\n", sizeof(dragon));
+	printf("Shape 구조체의 크기 : %d\n", sizeof(circle));
+
+	//멤버 변수를 메모리에서 CPU로 읽을 때 한 번에 읽을 수 있도록
+	//컴파일러가 레지스터의 블록에 맞추어 바이트를 패딩해주는 최적화작업
+
+	//align 규칙
+	//CPU가 데이터를 읽을때
+	//홀수(X) 3byte, 5byte/ 짝수(O) 2byte 4byte 8byte
+	//32bit OS에서는 4byte색 메모리를 읽고, 64bit OS에서는 8byte씩 메모리를 읽습니다.
+	//구조체의 크기는 구조체를 구성하는 멤버 중에
+	//크기가 가장 큰 자료혀으이 배수가 되도록 정렬합니다.
+	*/
+
+	//구조체 포인터
+	/*
+	//구조체를 가리키는 포인터
+
+	struct Animal* pointer;
+
+	printf("구조체 포인터의 크기 : %d\n", sizeof(pointer));
+
+	struct Animal cat;
+
+	//구조체 주소는 구조체 첫번째 멤버 변수에 있는 시작주소를
+	//의미합니다.
+	printf("구조체 cat의 주소 : %p\n", &cat);
+	printf("구조체 cat의 leg 주소 : %p\n", &cat.leg);
+	printf("구조체 cat의 size 주소 : %p\n", &cat.size);
+
+	pointer = &cat;
+
+	//구조체 포인터로 구조체에 있는 메모리에 접근할 때는
+	//-> 연산자를 사용해야 합니다.
+	pointer->leg = 200;
+	pointer->size = 90.3f;
+
+	printf("구조체 포인터로 접근한 leg의 값 : %d\n", pointer->leg);
+	printf("구조체 포인터로 접근한 leg의 값 : %.1lf\n", pointer->size);
+
+	//멤버 연산자를 사용하려면 연산자 우선 순위에 맞추어 작성하면 됨
+	(*pointer).leg = 400;
+	(*pointer).size = 50.6f;
+
+	printf("구조체 포인터로 접근한 leg의 값 : %d\n", pointer->leg);
+	printf("구조체 포인터로 접근한 leg의 값 : %.1lf\n", pointer->size);
+	*/
+
+	return 0;
+}
--- 소스.c
+++ 소스.c
@@ -1,103 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<string.h> //문자열 관련 함수
 
-struct Object
+//자기 참조 구조체
+/*
+struct Node
 {
-	short size; //2byte
-	int height; //4byte
-	double position; //8byte
+	int data;
+	struct Node* pointer;
 
 };
-
-struct Monster
-{
-	int healte; //4byte
-	double attack; //8byte
-	short defense; //2byte
-};
-
-struct Data
-{
-	int x; //4byte
-	int y; //4byte
-};
-
-struct Shape
-{
-	char name[10]; //10byte
-	double size; //8byte
-};
-
-struct Animal
-{
-	int leg;
-	double size;
-};
+*/
 
 int main()
 {
+	//자기 참조 구조체
+	/*
+	//자신과 동일한 구조체를 가리킬 수 있는 포인터 변수를
+	//멤버 변수로 가지는 구조체
+	//NULL은 포인터가 아무 위치를 가리키지 않도록 초기화 하는것
+
+	struct Node node1 = { 10, NULL };
+	struct Node node2 = { 20, NULL };
+	struct Node node3 = { 30, NULL };
+
+	struct Node* structPtr = &node1;
 	
-	//바이트 패딩
-	/*
-	//구조체의 경우 정의해주어야 멤버 변수의 메모리가 생기므로
-	//멤버 변수의 데이터를 초기화할 수 있습니다.
-	struct Object character = { 10, 100, 3.5f };
-	struct Data data = { 10, 10 };
+	printf("첫 번째 구조체 data의 값 : %d\n", structPtr->data);
 
-	//구조체 크기의 경우 멤버 변수의 순서에 따라 메모리의 크기가
-	//다르게 설정될 수 있으며, 구조체 크기를 결정하는 형태는
-	//기본 자료형으로만 구성됩니다.
-	struct Monster dragon = { 100,30.5f,10 };
-	struct Shape circle;
+	structPtr->pointer = &node2;
+	printf("두 번째 구조체 data의 값 : %d\n", structPtr->pointer->data);
 
-	printf("Object 구조체의 크기 : %d\n", sizeof(character));
-	printf("Data 구조체의 크기 : %d\n", sizeof(data));
-	printf("Monster 구조체의 크기 : %d\n", sizeof(dragon));
-	printf("Shape 구조체의 크기 : %d\n", sizeof(circle));
-
-	//멤버 변수를 메모리에서 CPU로 읽을 때 한 번에 읽을 수 있도록
-	//컴파일러가 레지스터의 블록에 맞추어 바이트를 패딩해주는 최적화작업
-
-	//align 규칙
-	//CPU가 데이터를 읽을때
-	//홀수(X) 3byte, 5byte/ 짝수(O) 2byte 4byte 8byte
-	//32bit OS에서는 4byte색 메모리를 읽고, 64bit OS에서는 8byte씩 메모리를 읽습니다.
-	//구조체의 크기는 구조체를 구성하는 멤버 중에
-	//크기가 가장 큰 자료혀으이 배수가 되도록 정렬합니다.
+	structPtr->pointer->pointer = &node3;
+	printf("세 번째 구조체 data의 값 : %d\n", structPtr->pointer->pointer->data);
 	*/
 
-	//구조체 포인터
+	//문자열 함수
+	
+	char name[] = { "asdf" };
+	printf("name의 값 : %s\n", name);
+	
+	//strlen - (문자열의 길이를 출력하는 함수)
+	printf("name배열의 길이 : %d\n",strlen(name));
+	//strlen()함수는 문자열의 길이를 구할 때 
+	//NULL문자 이전의 문자의 길이만 계산합니다.
+	
+	//strcpy - (문자열을 복사하는 함수)
+	char A[10] = { "qwer" };
+	char B[10] = { "zxvc" };
+
+	//read only 영역에 있는 문자열 리터럴이기 때문에 값을 변경할 수 없다.
+	const char* C = "Player";
+	const char* D = "Monster";
+
 	/*
-	//구조체를 가리키는 포인터
-
-	struct Animal* pointer;
-
-	printf("구조체 포인터의 크기 : %d\n", sizeof(pointer));
-
-	struct Animal cat;
-
-	//구조체 주소는 구조체 첫번째 멤버 변수에 있는 시작주소를
-	//의미합니다.
-	printf("구조체 cat의 주소 : %p\n", &cat);
-	printf("구조체 cat의 leg 주소 : %p\n", &cat.leg);
-	printf("구조체 cat의 size 주소 : %p\n", &cat.size);
-
-	pointer = &cat;
-
-	//구조체 포인터로 구조체에 있는 메모리에 접근할 때는
-	//-> 연산자를 사용해야 합니다.
-	pointer->leg = 200;
-	pointer->size = 90.3f;
-
-	printf("구조체 포인터로 접근한 leg의 값 : %d\n", pointer->leg);
-	printf("구조체 포인터로 접근한 leg의 값 : %.1lf\n", pointer->size);
-
-	//멤버 연산자를 사용하려면 연산자 우선 순위에 맞추어 작성하면 됨
-	(*pointer).leg = 400;
-	(*pointer).size = 50.6f;
-
-	printf("구조체 포인터로 접근한 leg의 값 : %d\n", pointer->leg);
-	printf("구조체 포인터로 접근한 leg의 값 : %.1lf\n", pointer->size);
+	int i = 0;
+	while (A[i])
+	{
+		A[i] = B[i];
+		i++;
+		printf("A의 문자열 : % s\n", A);
+	}
 	*/
+	
+	strcpy(A, B);
+	printf("복사한 A 문자열의 값 : %s", A);
+	
+	//strcpy()함수의 첫 번째 매개변수는 복사받을 문자열
+	//strcpy()함수의 두 번째 매개변수는 복사 할 문자열이다.
+
+	//strcmp - (문자열을 비교하는 함수)
+	//strcat - (문자열을 연결하는 함수)
+
+
+
+
+
 
 	return 0;
 }
